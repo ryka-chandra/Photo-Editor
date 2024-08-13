e1621a4d80afbd38a02aaeaa6033e8759f726fef
Ryka Chandra <ryka@seas.upenn.edu>
2024-08-13 01:54:44 -0400

    Update MyPixelTest.java

--- MyPixelTest.java
+++ MyPixelTest.java
@@ -3,30 +3,8 @@ package org.cis1200;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Use this file to test your implementation of Pixel.
- * 
- * We will manually grade this file to give you feedback
- * about the completeness of your test cases.
- */
 
 public class MyPixelTest {
-
-    /*
-     * Remember, UNIT tests should ideally have one point of failure. Below we
-     * give you two examples of unit tests for the Pixel constructor, one that
-     * takes in three ints as arguments and one that takes in an array. We use
-     * the getRed(), getGreen(), and getBlue() methods to check that the values
-     * were set correctly. These two tests do not comprehensively test all of
-     * Pixel so you must add your own.
-     * 
-     * You might want to look into assertEquals, assertTrue, assertFalse, and
-     * assertArrayEquals at the following:
-     * http://junit.sourceforge.net/javadoc/org/junit/Assert.html
-     *
-     * Note, if you want to add global variables so that you can reuse Pixels
-     * in multiple tests, feel free to do so.
-     */
 
     @Test
     public void testConstructInBounds() {
